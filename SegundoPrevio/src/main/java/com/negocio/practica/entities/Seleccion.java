d766b6e9570ee2dd8abcf551914fb7eace239556
HP <HP@DESKTOP-5KA18IE>
2023-05-29 12:55:33 -0500

    Otra actualizacion para ver los resultados

--- SegundoPrevio/src/main/java/com/negocio/practica/entities/Seleccion.java
+++ SegundoPrevio/src/main/java/com/negocio/practica/entities/Seleccion.java
@@ -3,6 +3,7 @@ package com.negocio.practica.entities;
 import java.util.List;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,7 +32,7 @@ public class Seleccion{
 
 	private String nombre;
 	
-	@OneToMany(mappedBy="seleccion")
+	@OneToMany(mappedBy="seleccion" , fetch = FetchType.EAGER)
 	private List<Resultado> resultados;
 
 	@ManyToOne
